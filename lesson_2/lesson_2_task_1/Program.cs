c67cd0b873e4f311de4bba6d0abdfa0ad4e1592c
Alaska <vacumsio@gmail.com>
2019-02-22 22:05:54 +0300

    ДЗ Lesson_2

---
+++ lesson_2/lesson_2_task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Написать метод, возвращающий минимальное из трёх чисел.*/
+
+namespace lesson_2_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 8;
+            var b = 15;
+            var c = 7;
+
+            Console.WriteLine(Math.Max(a, Math.Max(b, c)));
+            Console.ReadKey();
+        }
+    }
+}
