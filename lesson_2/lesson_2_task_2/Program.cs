3cf2bcdaa01236d252b68859faffd4699bd575f6
Alaska <vacumsio@gmail.com>
2019-02-25 11:00:36 +0300

    changes

--- lesson_2/lesson_2_task_2/Program.cs
+++ lesson_2/lesson_2_task_2/Program.cs
@@ -5,12 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*Написать метод подсчета количества цифр числа.*/
+
 namespace lesson_2_task_2
 {
     class Program
     {
+        public int Lenght()
+        {
+            
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число");
+            var x = Console.ReadLine();
+            Le
         }
     }
 }
