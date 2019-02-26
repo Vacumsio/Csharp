11429d8e448d82ab5638ce969b66333c51a743d7
Alaska <vacumsio@gmail.com>
2019-02-26 11:57:35 +0300

    add 3rd lesson

---
+++ lesson_3/lesson_3_task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_3_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
