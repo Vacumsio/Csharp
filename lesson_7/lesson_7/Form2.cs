b7c316646c06c18ea654a8672ad19961fa165a23
Alaska <vacumsio@gmail.com>
2019-03-13 17:53:22 +0300

    реализовано 1 задание в части "а" и "б"

---
+++ lesson_7/lesson_7/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson_7
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            btnStart1.Text = "Начать";
+            btnStart2.Text = "Начать";
+            lblMain.Text = "Главное меню";
+            lblGame1.Text = "Игра \"Удвоитель\"";
+            lblGame2.Text = "Игра \"Угадай число\"";
+        }
+
+
+        private void btnStart_1(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            MessageBox.Show("Получить 205 за 12 ходов");
+            f.ShowDialog();
+        }
+
+        private void btnStart_2(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
